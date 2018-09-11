5607df76cbdd66ffd479a6fc9c5be06f88510c35
Romeu.Wanderley <romeu.a.w.1964@gmail.com>
2018-09-11 17:45:57 -0300

    Commit para o PUSH

--- projeto2.py
+++ projeto2.py
@@ -1,2 +1,12 @@
+lista = [1,2,3,4,5]
+it = iter(lista)
+while True:
+    try:
+        e = next(it)
+        print(e)
+    except StopIteration:
+        print("Não há mais elementos")
+        break
 
-    # 1 2 3 4 5 6
+
+
